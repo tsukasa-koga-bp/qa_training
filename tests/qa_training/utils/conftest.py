f5b204642ed280fca811fd372a10df13c7c600cb
eiki_kitajima <eiki.kitajima@brainpad.co.jp>
2023-04-18 07:34:57 +0000

    test: config_managerのテスト追加 fix: config_managerのバグ修正

---
+++ tests/qa_training/utils/conftest.py
@@ -0,0 +1,87 @@
+import pytest
+from _pytest.fixtures import SubRequest
+from py.xml import html
+from qa_training.utils.config_manager import (
+    ConfigManager,
+    ConfigManagerRepoCommand,
+    ConfigManagerUsecaseCommand,
+)
+
+params_yaml = {
+    "default": (
+        ConfigManagerUsecaseCommand(
+            usecase_create_model_yaml_path="configs/usecase/UsecaseCreateModel.yaml",
+            usecase_judge_survival_yaml_path="configs/usecase/UsecaseJudgeSurvival.yaml",
+        ),
+        ConfigManagerRepoCommand(
+            repo_raw_data_yaml_path="configs/repo/RepoRawData.yaml",
+            repo_cleansed_data_yaml_path="configs/repo/RepoCleansedData.yaml",
+            repo_features_yaml_path="configs/repo/RepoFeatures.yaml",
+            repo_model_yaml_path="configs/repo/RepoModel.yaml",
+        ),
+    ),
+}
+
+
+@pytest.fixture(
+    params=list(params_yaml.values()),
+    ids=list(params_yaml.keys()),
+)
+def commands(
+    request: SubRequest,
+) -> tuple[ConfigManagerUsecaseCommand, ConfigManagerRepoCommand]:
+    usecase_command = request.param[0]
+    repo_command = request.param[1]
+    return usecase_command, repo_command
+
+
+@pytest.fixture
+def config_manager(
+    commands: tuple[ConfigManagerUsecaseCommand, ConfigManagerRepoCommand]
+) -> ConfigManager:
+    usecase_command = commands[0]
+    repo_command = commands[1]
+    config_manager = ConfigManager(
+        usecase_command=usecase_command, repo_command=repo_command
+    )
+    return config_manager
+
+
+# @pytest.fixture
+# def domain_registry(config_manager: ConfigManager) -> DomainRegistry:
+#    return DomainRegistry(config_manager=config_manager)
+
+
+def pytest_html_report_title(report):
+    report.title = "テスト仕様書"
+
+
+def pytest_html_results_table_header(cells):
+    del cells[1]
+    cells.insert(1, html.td("TestFolder"))
+    cells.insert(2, html.td("TestScript"))
+    cells.insert(3, html.td("TestFunction"))
+    cells.insert(4, html.th("Description"))
+    cells.insert(5, html.th("Parameter"))
+    cells.pop()
+
+
+def pytest_html_results_table_row(report, cells):
+    del cells[1]
+    cells.insert(1, html.td(report.test_folder))
+    cells.insert(2, html.td(report.test_script))
+    cells.insert(3, html.td(report.test_func))
+    cells.insert(4, html.td(report.description))
+    cells.insert(5, html.td(report.param))
+    cells.pop()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    report.test_folder = item.fspath.dirname
+    report.test_script = item.fspath.basename
+    report.test_func = item.originalname
+    report.description = str(item.function.__doc__)
+    report.param = item.name.removeprefix(item.originalname)
