fb2c6fde5956c4016b9b8304f3dc8d99d0ba2574
eiki_kitajima <eiki.kitajima@brainpad.co.jp>
2023-04-18 02:00:52 +0000

    rem: 不用なコード削除

--- src/qa_training/temp/temp.py
+++
@@ -1,7 +0,0 @@
-class Temp:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def greet(self):
-        print(f"Hello, my name is {self.name} and I'm {self.age} years old.")
