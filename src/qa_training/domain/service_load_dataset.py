ddcdc78af9b0faaae8556eb3337562c02369678a
majitaki4ryu <113817691+majitaki4ryu@users.noreply.github.com>
2023-04-24 18:44:43 +0000

    feat: データ読み込み処理実装 test: データ読み込み処理テスト追加

--- src/qa_training/domain/service_load_dataset.py
+++ src/qa_training/domain/service_load_dataset.py
@@ -3,4 +3,8 @@ import pandas as pd
 
 class ServiceLoadDataset:
     def run(self, csv_path: str) -> pd.DataFrame:
-        return pd.DataFrame()
+        df_customer_info = pd.read_csv(
+            csv_path,
+            sep="\t",
+        )
+        return df_customer_info
--- tests/qa_training/domain/service_load_dataset/test_service_load_dataset.py
+++ tests/qa_training/domain/service_load_dataset/test_service_load_dataset.py
@@ -7,8 +7,11 @@ from qa_training.utils.my_assert_frame_equal import MyAssert
 @pytest.fixture
 def fixture_run():
     service_load_dataset = ServiceLoadDataset()
-    csv_path = ""
-    df_customer_info_expected = pd.DataFrame()
+    csv_path = "./tests/qa_training/domain/service_load_dataset/data/train.csv"
+    df_customer_info_expected = pd.read_csv(
+        "./tests/qa_training/domain/service_load_dataset/data/train.csv",
+        sep="\t",
+    )
 
     return service_load_dataset, csv_path, df_customer_info_expected
 
