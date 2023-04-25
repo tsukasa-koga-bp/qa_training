11d6b8a889e592f2ef6bcb62aaf90b7cd32205a5
majitaki4ryu <113817691+majitaki4ryu@users.noreply.github.com>
2023-04-25 00:42:16 +0000

    feat: 特徴量作成を実装 test: テストデータ更新

--- src/qa_training/domain/service_make_features.py
+++ src/qa_training/domain/service_make_features.py
@@ -5,27 +5,55 @@ import pandas as pd
 
 class ServiceMakeFeatures:
     def run(self, df_customer_info: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
-        df_X = self._make_X(df_customer_info)
-        df_y = self._make_y(df_customer_info)
+        df_X, df_id = self._make_X(df_customer_info)
+        df_y = self._make_y(df_id=df_id, df_customer_info=df_customer_info)
         return df_X, df_y
 
-    def _make_X(self, df_customer_info: pd.DataFrame) -> pd.DataFrame:
-        # df_X = pd.get_dummies(df_customer_info[["Pclass", "Sex", "SibSp", "Parch"]])
-        df_X = df_customer_info[["Pclass", "Sex", "SibSp", "Parch"]]
-        return df_X
+    def _make_X(
+        self, df_customer_info: pd.DataFrame
+    ) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        # 欠損値補完
+        df_filled = self._handle_missing_values(df_customer_info=df_customer_info)
 
-    def _make_y(self, df_customer_info: pd.DataFrame) -> pd.DataFrame:
-        df_y = df_customer_info[["Survived"]]
+        # 制約違反の行を捨てる
+        df_obeyed = self._handle_violations(df_filled=df_filled)
+
+        # 特徴量作成
+        df_X_and_id = self._make_features(df_obeyed=df_obeyed)
+
+        df_X = df_X_and_id.drop("PassengerId", axis=1)
+        df_id = df_X_and_id[["PassengerId"]]
+
+        return df_X, df_id
+
+    def _make_y(
+        self, df_id: pd.DataFrame, df_customer_info: pd.DataFrame
+    ) -> pd.DataFrame:
+        df_y = pd.merge(df_id, df_customer_info, on="PassengerId", how="left")
+        df_y = df_y[["Survived"]]
+
         return df_y
 
-    """
-    def _make_survived(self, df_origin: pd.DataFrame):
-        pass
+    def _handle_missing_values(self, df_customer_info) -> pd.DataFrame:
+        df_customer_info["Age"] = df_customer_info["Age"].fillna(10)
+        df_customer_info["Cabin"] = df_customer_info["Cabin"].fillna("S")
+        df_customer_info = df_customer_info.dropna()
+        return df_customer_info
 
-    def _make_familiy_size(self, df_origin: pd.DataFrame) -> pd.DataFrame:
-        return pd.DataFrame()
+    def _handle_violations(self, df_filled) -> pd.DataFrame:
+        df_filled = df_filled[df_filled["Pclass"].isin([1, 2, 3])]
+        df_filled = df_filled[df_filled["Sex"].isin(["male", "female"])]
+        df_filled = df_filled[
+            (df_filled["Age"] >= 0) & (df_filled["Age"].apply(float.is_integer))
+        ]
+        df_filled = df_filled[df_filled["Embarked"].isin(["C", "Q", "S"])]
 
-    def _make_is_alone(self, df_origin: pd.DataFrame) -> pd.DataFrame:
-        return pd.DataFrame()
+        return df_filled
 
-    """
+    def _make_features(self, df_obeyed: pd.DataFrame) -> pd.DataFrame:
+        df_obeyed = df_obeyed[
+            ["PassengerId", "Sex", "Embarked", "Pclass", "Age", "Fare"]
+        ]
+        df_obeyed["Sex"] = df_obeyed["Sex"].replace({"male": 0, "female": 1})
+        df_obeyed = pd.get_dummies(df_obeyed, columns=["Embarked"], dtype=float)
+        return df_obeyed
