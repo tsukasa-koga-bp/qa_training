9570423daa25e195be941df05260c17004e36257
majitaki4ryu <113817691+majitaki4ryu@users.noreply.github.com>
2023-04-27 21:45:12 +0000

    test: handle_missing_valuesに変更

--- tests/qa_training/domain/service_make_features_data/test_handle_missing_values.py
+++ tests/qa_training/domain/service_make_features_data/test_handle_missing_values.py
@@ -5,21 +5,21 @@ from qa_training.utils.my_assert_frame_equal import MyAssert
 
 
 @pytest.fixture
-def fixture_missing_values_in_sex():
+def fixture_handle_missing_values_in_sex():
     service_make_features = ServiceMakeFeatures()
 
     df_customer_info = pd.read_csv(
-        "tests/qa_training/domain/service_make_features_data/missing_values_in_sex/df_customer_info.csv",
+        "tests/qa_training/domain/service_make_features_data/handle_missing_values_in_sex/df_customer_info.csv",
     )
     df_filled_expected = pd.read_csv(
-        "tests/qa_training/domain/service_make_features_data/missing_values_in_sex/df_filled_expected.csv",
+        "tests/qa_training/domain/service_make_features_data/handle_missing_values_in_sex/df_filled_expected.csv",
     )
 
     return (service_make_features, df_customer_info, df_filled_expected)
 
 
-def test_missing_values_in_sex(
-    fixture_missing_values_in_sex: tuple[
+def test_handle_missing_values_in_sex(
+    fixture_handle_missing_values_in_sex: tuple[
         ServiceMakeFeatures, pd.DataFrame, pd.DataFrame
     ]
 ):
@@ -29,7 +29,7 @@ def test_missing_values_in_sex(
         service_make_features,
         df_customer_info,
         df_filled_expected,
-    ) = fixture_missing_values_in_sex
+    ) = fixture_handle_missing_values_in_sex
 
     df_filled = service_make_features._handle_missing_values(df_customer_info)
 
@@ -37,21 +37,21 @@ def test_missing_values_in_sex(
 
 
 @pytest.fixture
-def fixture_missing_values_in_age():
+def fixture_handle_missing_values_in_age():
     service_make_features = ServiceMakeFeatures()
 
     df_customer_info = pd.read_csv(
-        "tests/qa_training/domain/service_make_features_data/missing_values_in_age/df_customer_info.csv",
+        "tests/qa_training/domain/service_make_features_data/handle_missing_values_in_age/df_customer_info.csv",
     )
     df_filled_expected = pd.read_csv(
-        "tests/qa_training/domain/service_make_features_data/missing_values_in_age/df_filled_expected.csv",
+        "tests/qa_training/domain/service_make_features_data/handle_missing_values_in_age/df_filled_expected.csv",
     )
 
     return (service_make_features, df_customer_info, df_filled_expected)
 
 
-def test_missing_values_in_age(
-    fixture_missing_values_in_age: tuple[
+def test_handle_missing_values_in_age(
+    fixture_handle_missing_values_in_age: tuple[
         ServiceMakeFeatures, pd.DataFrame, pd.DataFrame
     ]
 ):
@@ -61,7 +61,7 @@ def test_missing_values_in_age(
         service_make_features,
         df_customer_info,
         df_filled_expected,
-    ) = fixture_missing_values_in_age
+    ) = fixture_handle_missing_values_in_age
 
     df_filled = service_make_features._handle_missing_values(df_customer_info)
 
@@ -69,21 +69,21 @@ def test_missing_values_in_age(
 
 
 @pytest.fixture
-def fixture_missing_values_in_embarked():
+def fixture_handle_missing_values_in_embarked():
     service_make_features = ServiceMakeFeatures()
 
     df_customer_info = pd.read_csv(
-        "tests/qa_training/domain/service_make_features_data/missing_values_in_embarked/df_customer_info.csv",
+        "tests/qa_training/domain/service_make_features_data/handle_missing_values_in_embarked/df_customer_info.csv",
     )
     df_filled_expected = pd.read_csv(
-        "tests/qa_training/domain/service_make_features_data/missing_values_in_embarked/df_filled_expected.csv",
+        "tests/qa_training/domain/service_make_features_data/handle_missing_values_in_embarked/df_filled_expected.csv",
     )
 
     return (service_make_features, df_customer_info, df_filled_expected)
 
 
-def test_missing_values_in_embarked(
-    fixture_missing_values_in_embarked: tuple[
+def test_handle_missing_values_in_embarked(
+    fixture_handle_missing_values_in_embarked: tuple[
         ServiceMakeFeatures, pd.DataFrame, pd.DataFrame
     ]
 ):
@@ -93,7 +93,7 @@ def test_missing_values_in_embarked(
         service_make_features,
         df_customer_info,
         df_filled_expected,
-    ) = fixture_missing_values_in_embarked
+    ) = fixture_handle_missing_values_in_embarked
 
     df_filled = service_make_features._handle_missing_values(df_customer_info)
 
@@ -101,21 +101,21 @@ def test_missing_values_in_embarked(
 
 
 @pytest.fixture
-def fixture_missing_values_in_pclass():
+def fixture_handle_missing_values_in_pclass():
     service_make_features = ServiceMakeFeatures()
 
     df_customer_info = pd.read_csv(
-        "tests/qa_training/domain/service_make_features_data/missing_values_in_pclass/df_customer_info.csv",
+        "tests/qa_training/domain/service_make_features_data/handle_missing_values_in_pclass/df_customer_info.csv",
     )
     df_filled_expected = pd.read_csv(
-        "tests/qa_training/domain/service_make_features_data/missing_values_in_pclass/df_filled_expected.csv",
+        "tests/qa_training/domain/service_make_features_data/handle_missing_values_in_pclass/df_filled_expected.csv",
     )
 
     return (service_make_features, df_customer_info, df_filled_expected)
 
 
-def test_missing_values_in_pclass(
-    fixture_missing_values_in_pclass: tuple[
+def test_handle_missing_values_in_pclass(
+    fixture_handle_missing_values_in_pclass: tuple[
         ServiceMakeFeatures, pd.DataFrame, pd.DataFrame
     ]
 ):
@@ -125,7 +125,7 @@ def test_missing_values_in_pclass(
         service_make_features,
         df_customer_info,
         df_filled_expected,
-    ) = fixture_missing_values_in_pclass
+    ) = fixture_handle_missing_values_in_pclass
 
     df_filled = service_make_features._handle_missing_values(df_customer_info)
 
@@ -133,21 +133,21 @@ def test_missing_values_in_pclass(
 
 
 @pytest.fixture
-def fixture_missing_values_in_other():
+def fixture_handle_missing_values_in_other():
     service_make_features = ServiceMakeFeatures()
 
     df_customer_info = pd.read_csv(
-        "tests/qa_training/domain/service_make_features_data/missing_values_in_other/df_customer_info.csv",
+        "tests/qa_training/domain/service_make_features_data/handle_missing_values_in_other/df_customer_info.csv",
     )
     df_filled_expected = pd.read_csv(
-        "tests/qa_training/domain/service_make_features_data/missing_values_in_other/df_filled_expected.csv",
+        "tests/qa_training/domain/service_make_features_data/handle_missing_values_in_other/df_filled_expected.csv",
     )
 
     return (service_make_features, df_customer_info, df_filled_expected)
 
 
-def test_missing_values_in_other(
-    fixture_missing_values_in_other: tuple[
+def test_handle_missing_values_in_other(
+    fixture_handle_missing_values_in_other: tuple[
         ServiceMakeFeatures, pd.DataFrame, pd.DataFrame
     ]
 ):
@@ -159,7 +159,7 @@ def test_missing_values_in_other(
         service_make_features,
         df_customer_info,
         df_filled_expected,
-    ) = fixture_missing_values_in_other
+    ) = fixture_handle_missing_values_in_other
 
     df_filled = service_make_features._handle_missing_values(df_customer_info)
 
